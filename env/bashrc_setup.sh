e899847a126834adda722dee44cac151e0f743e2
Hurley <hurleyLi@users.noreply.github.com>
2021-04-14 09:43:12 -0500

    Create bashrc_setup.sh

---
+++ env/bashrc_setup.sh
@@ -0,0 +1,8 @@
+alias ?="ls -lrth"
+alias sn="screen"
+alias sr="screen -r"
+alias less="less -S"
+alias zless="zless -S"
+
+PS1="\e[0;32m\w\e[0m  \e[1;36m>> \e[0m"
+LS_COLORS=$LS_COLORS:'di=01;94:'; export LS_COLORS
