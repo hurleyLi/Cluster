25661545e13344ea3a15264db261a19bedc3f3a0
Hurley <hurleyLi@users.noreply.github.com>
2021-04-14 09:19:24 -0500

    Create 00-first.py

---
+++ ipython_startup/00-first.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pickle
+import sys
+import os
+import rpy2.rinterface
+
+pd.set_option("display.max_rows", 500)
+pd.set_option("display.max_columns", 500)
+sns.set(style="white")
+
+params = {'legend.fontsize': 22,
+          'figure.figsize': (10, 7),
+          'axes.labelsize': 22,
+          'axes.titlesize':22,
+          'xtick.labelsize':22,
+          'ytick.labelsize':22}
+plt.rcParams.update(params)
+
+# %load_ext rpy2.ipython
+# %matplotlib inline
