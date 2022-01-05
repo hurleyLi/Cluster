427398b08843c74707cf9f7a10d987b8571b662b
Hurley <hurleyLi@users.noreply.github.com>
2022-01-05 09:11:22 -0500

    Update bashrc_setup.sh

--- env/bashrc_setup.sh
+++ env/bashrc_setup.sh
@@ -3,6 +3,7 @@ alias sn="screen"
 alias sr="screen -r"
 alias less="less -S"
 alias zless="zless -S"
+alias install_hsc="pip install git+https://github.com/hurleyLi/single-cell.git"
 
 PS1="\e[0;32m\w\e[0m  \e[1;36m>> \e[0m"
 LS_COLORS=$LS_COLORS:'di=01;94:'; export LS_COLORS
