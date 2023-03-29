cd26e3f0c8bd19ab47a5e5dfca94735059af875e
Hurley <hurleyLi@users.noreply.github.com>
2023-03-29 09:54:30 -0400

    Update bashrc_setup.sh

--- env/bashrc_setup.sh
+++ env/bashrc_setup.sh
@@ -3,11 +3,11 @@ alias sn="screen"
 alias sr="screen -r"
 alias less="less -S"
 alias zless="zless -S"
-alias install_hsc="sudo pip uninstall hurleysc; sudo pip install git+https://github.com/hurleyLi/single-cell.git"
+alias install_hsc="sudo pip uninstall hurleysc; pip install git+https://github.com/hurleyLi/single-cell.git --user"
 alias git_all="git add .; git commit -m'update'; git push"
 
-export PYTHON_SITE="/opt/conda/lib/python3.8/site-packages"
-export HSC_TEMPLATES="/opt/conda/lib/python3.8/site-packages/hurleysc/nb_templates"
+export PYTHON_SITE="/opt/conda/lib/python3.9/site-packages"
+export HSC_TEMPLATES="/opt/conda/lib/python3.9/site-packages/hurleysc/nb_templates"
 
 PS1="\e[0;32m\w\e[0m  \e[1;36m>> \e[0m"
 LS_COLORS=$LS_COLORS:'di=01;94:'; export LS_COLORS
