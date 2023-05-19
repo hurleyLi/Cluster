3fdd8c3e19ed8f3950b4f1463c5ea1097c112b37
Hurley <hurleyLi@users.noreply.github.com>
2023-05-18 23:11:57 -0400

    Update bashrc_setup.sh

--- env/bashrc_setup.sh
+++ env/bashrc_setup.sh
@@ -3,7 +3,8 @@ alias sn="screen"
 alias sr="screen -r"
 alias less="less -S"
 alias zless="zless -S"
-alias install_hsc="sudo pip uninstall hurleysc; pip uninstall hurleysc; pip install git+https://github.com/hurleyLi/single-cell.git --user"
+alias install_hsc="sudo pip uninstall hurleysc; pip uninstall hurleysc; pip install git+https://${github_token}@github.com/hurleyLi/single-cell.git --user"
+# pip install git+https://github.com/hurleyLi/single-cell.git 
 alias git_all="git add .; git commit -m'update'; git push"
 
 export PYTHON_SITE="/opt/conda/lib/python3.9/site-packages"
