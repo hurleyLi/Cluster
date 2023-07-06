48948a11d4c0484e568e4bcc37e5cf38ceecbba8
Hurley <hurleyLi@users.noreply.github.com>
2023-07-06 12:01:55 -0400

    Update bashrc_setup.sh

--- env/bashrc_setup.sh
+++ env/bashrc_setup.sh
@@ -11,6 +11,7 @@ export PYTHON_SITE=$(pip show hurleysc | awk '/^Location: / {print $2}')
 export HSC_DIR=$PYTHON_SITE/hurleysc
 export HSC_TEMPLATES=$HSC_DIR/nb_templates
 export HSC_RESOURCE=$HSC_DIR/resource
+export GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no"
 
 PS1="\e[0;32m\w\e[0m  \e[1;36m>> \e[0m"
 LS_COLORS=$LS_COLORS:'di=01;94:'; export LS_COLORS
